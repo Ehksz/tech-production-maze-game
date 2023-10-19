8b703290e500f08841982d9c2d590ed57fa8ae01
xavier <xavier@studiolabs.com>
2023-10-19 19:12:15 -0400

    merged su, marlen and edwards work.

--- maze-game/src/main.js
+++ maze-game/src/main.js
@@ -1,45 +1,51 @@
-import './style.css'
-import Phaser from 'phaser'
+import "./style.css";
+import Phaser from "phaser";
 
 const sizes = {
-  width: 500,
-  height: 500
-}
-const speedDown = 300
+  width: 1920,
+  height: 1080,
+};
+const speedDown = 300;
 
-class GameScene extends Phaser.Scene{
+class GameScene extends Phaser.Scene {
   constructor() {
-    super("scene-game")
-    this.player
+    super("scene-game");
+    this.player;
   }
-  preload(){
-    this.load.image("theplayer", "/assets/theplayer.png")
+  preload() {
+    this.load.image("theplayer", "/assets/theplayer.png");
     this.load.image("bg", "/assets/bg.png");
   }
-  create(){
-    this.add.image(0, 0, "bg").setOrigin(0,0);
+  create() {
+    this.add.image(0, 0, "bg").setOrigin(0, 0);
+    this.player = this.add
+      .image(sizes.width - 300, sizes.height - 300, "theplayer")
+      .setOrigin(0, 0);
 
-    this.player = this.physics.add.image(sizes.width-300, sizes.height-300, "theplayer").setOrigin(0,0)
-    this.player.setImmovable(true)
-    this.player.body.allowGravity = false
+    this.player.scale = 0.5;
+    const cursorKeys = this.input.keyboard?.createCursorKeys();
+    this.update = () => {
+      if (cursorKeys?.up.isDown) {
+        this.player.y -= 2;
+      }
+    };
   }
-  update(){}
-} 
-
+  update() {}
+}
 
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
   canvas: gameCanvas,
-  physics:{
-    default:"arcade",
+  physics: {
+    default: "arcade",
     arcade: {
-      gravity:{y:speedDown},
-      debug: true
-    }
+      gravity: { y: speedDown },
+      debug: true,
+    },
   },
-  scene:[GameScene]
-}
+  scene: [GameScene],
+};
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
