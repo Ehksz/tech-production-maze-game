7ae268276e5bed7d427a07bc63604cf9befc7328
Carlos X. Viera <cx@viera.io>
2023-11-16 18:23:45 -0500

    Xavier-branch (#14)

---
+++ assignments.md
@@ -0,0 +1,8 @@
+1. Research on generating random mazes.
+2. All images preloaded in a separate class. (link)[https://github.com/photonstorm/phaser3-examples/tree/master/public/src/games/bank%20panic]
+3. Turning the player as it moves. left, right, up, down, top-right, top-left, bottom-left, bottom right.
+4. Start Game Scene.
+5. End Game Scene.
+6. Mouse Clicks.
+7. The background needs to fully cover the screen.
+8. Gravesends Inn Website Images.
--- maze-game/src/main.js
+++ maze-game/src/main.js
@@ -15,27 +15,15 @@ class GameScene extends Phaser.Scene {
     this.cursor;
     this.playerSpeed = speedDown + 50;
     this.object;//template to asiign the mutlple or single object/asset
-
+    this.isMoving = false;
   }
 
   preload(){
     this.load.image("theplayer", "/assets/theplayer.png");
     this.load.image("apple", "assets/apple.png");
     this.load.image("bg", "/assets/bg.png");
-
-    //glowing asset
-    this.load.image('apple-0','/assets/glowing-asset/apple-glow.png');
-    this.load.image('bag','/assets/glowing-asset/bag-glow.png');
-    this.load.image('book','/assets/glowing-asset/book-glow.png');
-    this.load.image('cauldron','/assets/glowing-asset/cauldron-glow.png');
-    this.load.image('chest','/assets/glowing-asset/chest-glow.png');
-    this.load.image('clock','/assets/glowing-asset/clock-glow.png');
-    this.load.image('feather-quill','/assets/glowing-asset/feather-quill-glow.png');
-    this.load.image('horn','/assets/glowing-asset/horn-glow.png');
-    this.load.image('pile','/assets/glowing-asset/pile-glow.png');
-    this.load.image('rat','/assets/glowing-asset/rat-glow.png');
-    this.load.image('red-skull','/assets/glowing-asset/red-skull-glow.png');
-    this.load.image('torch','/assets/glowing-asset/torch-glow.png');
+    this.load.audio('bg-audio', "/assets/dead.mp3");
+    this.load.audio('soundMove', "/assets/soundMove.mp3");
   }
 
   create(){
@@ -56,13 +44,19 @@ class GameScene extends Phaser.Scene {
 
 
     //player code
+    this.soundMove = this.sound.add("soundMove", {
+      volume: 1,
+  });
     this.player = this.physics.add.image(sizes.width-300, sizes.height-300, "theplayer").setOrigin(0,0)
+    this.player.rotation()
     this.player.body.allowGravity = false
     this.player.setCollideWorldBounds(true);
     //keyboard movement for player testing
     this.cursor = this.input.keyboard.createCursorKeys();
-    
-
+        //background audio
+    this.bgMusic = this.sound.add('bg-audio');
+    // this.bgMusic.play();
+        
     //template for object/asset hitbox
     this.object = this.physics.add.image(sizes.width - 300, sizes.height - 300, "apple").setOrigin(0, 0);
     this.object.setCollideWorldBounds(true);
@@ -74,42 +68,43 @@ class GameScene extends Phaser.Scene {
    this.physics.add.collider(this.player, this.object);
    this.add.image(0, 0, "bg").setOrigin(0, 0);
    this.player.scale = 0.5;
+   this.soundMove.play()
   }
+  
   //my player controls for testing using keyboard
   update(){
-    const { left, right, up, down } = this.cursor;
 
-    if (left.isDown) {
-      this.player.setVelocityX(-this.playerSpeed);
-    } else if (right.isDown) {
-      this.player.setVelocityX(this.playerSpeed);
-    } else {
-      this.player.setVelocityX(0);
+    if(typeof this.cursor !== 'undefined') {
+      const { left, right, up, down } = this.cursor;
+
+      if (left.isDown) {
+        this.isMoving = true;
+        this.player.setVelocityX(-this.playerSpeed);
+      } else if (right.isDown) {
+        this.isMoving = true;
+        this.player.setVelocityX(this.playerSpeed);
+      } else {
+        this.isMoving = false;
+        this.player.setVelocityX(0);
+      }
+      
+      if (up.isDown) {
+        this.isMoving = true;
+        this.player.setVelocityY(-this.playerSpeed);
+      } else if (down.isDown) {
+        this.isMoving = true;
+        this.player.setVelocityY(this.playerSpeed);
+      } else {
+        this.player.setVelocityY(0);
+        this.isMoving = false;
+      }
     }
-    
-    if (up.isDown) {
-      this.player.setVelocityY(-this.playerSpeed);
-    } else if (down.isDown) {
-      this.player.setVelocityY(this.playerSpeed);
+
+    if(this.isMoving) {
+      this.soundMove.setVolume(1)
     } else {
-      this.player.setVelocityY(0);
+      this.soundMove.setVolume(0);
     }
-
-
-    // This is main code
-    // const cursorKeys = this.input.keyboard?.createCursorKeys();
-    // if (cursorKeys?.up.isDown) {
-    //   this.player.y -= 2;
-    // }
-    // else if(cursorKeys?.down.isDown){
-    //   this.player.y +=2;
-    // }
-    // else if(cursorKeys?.left.isDown){
-    //   this.player.x -=2;
-    // }
-    // else if(cursorKeys?.right.isDown){
-    //   this.player.x +=2;
-    // }
 
     // for loop create 3 square top each other and beside.
     const size = 40;
