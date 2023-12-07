5f92a547153f997196c96e3e66b3ec9ab20b10c1
xavier <xavier@studiolabs.com>
2023-12-07 15:02:17 -0500

    fix merge conflicts

--- maze-game/src/main.js
+++ maze-game/src/main.js
@@ -1,9 +1,9 @@
 import "./style.css";
 import Phaser from "phaser";
-import Preloader from './preloader';
-import Start from './start';
-import GameOver from './gameover';
-import GameWin from './gamewin';
+import Preloader from "./preloader";
+import Start from "./start";
+import GameOver from "./gameover";
+import GameWin from "./gamewin";
 
 const sizes = {
   width: 1920,
@@ -14,55 +14,57 @@ const speedDown = 400;
 
 class GameScene extends Phaser.Scene {
   constructor() {
-    super("Maze")
+    super("Maze");
     this.player;
     this.cursor;
     this.playerSpeed = speedDown + 50;
-    this.object;//template to asiign the mutlple or single object/asset
+    this.object; //template to asiign the mutlple or single object/asset
     this.isMoving = false;
     this.spaceBar;
     this.isJumping = false;
     this.jumpHeight = 100;
-    this.orignalJumpSpot;//the oringal y axis when the player initalize the jump 
+    this.orignalJumpSpot; //the oringal y axis when the player initalize the jump
   }
 
   preload() {
     this.load.image("theplayer", "/assets/theplayer.png");
     this.load.image("apple", "assets/apple.png");
     this.load.image("background", "/assets/bg.png");
-    this.load.audio('bg-audio', "/assets/dead.mp3");
-    this.load.audio('soundMove', "/assets/soundMove.mp3");
+    this.load.audio("bg-audio", "/assets/dead.mp3");
+    this.load.audio("soundMove", "/assets/soundMove.mp3");
 
     //glowing asset
-    this.load.image('apple-0', '/assets/glowing-asset/apple-glow.png');
-    this.load.image('bag', '/assets/glowing-asset/bag-glow.png');
-    this.load.image('book', '/assets/glowing-asset/book-glow.png');
-    this.load.image('cauldron', '/assets/glowing-asset/cauldron-glow.png');
-    this.load.image('chest', '/assets/glowing-asset/chest-glow.png');
-    this.load.image('clock', '/assets/glowing-asset/clock-glow.png');
-    this.load.image('feather-quill', '/assets/glowing-asset/feather-quill-glow.png');
-    this.load.image('horn', '/assets/glowing-asset/horn-glow.png');
-    this.load.image('pile', '/assets/glowing-asset/pile-glow.png');
-    this.load.image('rat', '/assets/glowing-asset/rat-glow.png');
-    this.load.image('red-skull', '/assets/glowing-asset/red-skull-glow.png');
-    this.load.image('torch', '/assets/glowing-asset/torch-glow.png');
-    this.load.audio('bg-audio', "/assets/dead.mp3");
+    this.load.image("apple-0", "/assets/glowing-asset/apple-glow.png");
+    this.load.image("bag", "/assets/glowing-asset/bag-glow.png");
+    this.load.image("book", "/assets/glowing-asset/book-glow.png");
+    this.load.image("cauldron", "/assets/glowing-asset/cauldron-glow.png");
+    this.load.image("chest", "/assets/glowing-asset/chest-glow.png");
+    this.load.image("clock", "/assets/glowing-asset/clock-glow.png");
+    this.load.image(
+      "feather-quill",
+      "/assets/glowing-asset/feather-quill-glow.png"
+    );
+    this.load.image("horn", "/assets/glowing-asset/horn-glow.png");
+    this.load.image("pile", "/assets/glowing-asset/pile-glow.png");
+    this.load.image("rat", "/assets/glowing-asset/rat-glow.png");
+    this.load.image("red-skull", "/assets/glowing-asset/red-skull-glow.png");
+    this.load.image("torch", "/assets/glowing-asset/torch-glow.png");
+    this.load.audio("bg-audio", "/assets/dead.mp3");
 
     //Christian - Artifiacts
-    this.load.image('apple-1', '/assets/christian-artifacts/apple.png');
-    this.load.image('bag-0', '/assets/christian-artifacts/bag.png');
-    this.load.image('book-0', '/assets/christian-artifacts/book.png');
-    this.load.image('cauldron-0', '/assets/christian-artifacts/cauldron.png');
-    this.load.image('chest-0', '/assets/christian-artifacts/chest.png');
-    this.load.image('clock-0', '/assets/christian-artifacts/clock.png');
-    this.load.image('feather-0', '/assets/christian-artifacts/feather.png');
-    this.load.image('horn-0', '/assets/christian-artifacts/horn.png');
-    this.load.image('pile-0', '/assets/christian-artifacts/pile.png');
-    this.load.image('rat-0', '/assets/christian-artifacts/rat.png');
-    this.load.image('red-skull-0', '/assets/christian-artifacts/red-skull.png');
-    this.load.image('torch-0', '/assets/christian-artifacts/torch.png');
+    this.load.image("apple-1", "/assets/christian-artifacts/apple.png");
+    this.load.image("bag-0", "/assets/christian-artifacts/bag.png");
+    this.load.image("book-0", "/assets/christian-artifacts/book.png");
+    this.load.image("cauldron-0", "/assets/christian-artifacts/cauldron.png");
+    this.load.image("chest-0", "/assets/christian-artifacts/chest.png");
+    this.load.image("clock-0", "/assets/christian-artifacts/clock.png");
+    this.load.image("feather-0", "/assets/christian-artifacts/feather.png");
+    this.load.image("horn-0", "/assets/christian-artifacts/horn.png");
+    this.load.image("pile-0", "/assets/christian-artifacts/pile.png");
+    this.load.image("rat-0", "/assets/christian-artifacts/rat.png");
+    this.load.image("red-skull-0", "/assets/christian-artifacts/red-skull.png");
+    this.load.image("torch-0", "/assets/christian-artifacts/torch.png");
   }
-
 
   create(data) {
     // Create the maze - Professor will provided the assets in a commit.
@@ -70,7 +72,7 @@ class GameScene extends Phaser.Scene {
     // Each asset should have a collision attached to it.
     // When user escapes the maze transition to the game win,
     // Counter that starts 1 minute [60, 59, 58, 57, etc] when its hits zero transition to game over.
-  
+
     // We'll also add assets to make it look nice.
     // When Time runs out transition to the the game over.
     // var background = data.background;
@@ -84,128 +86,131 @@ class GameScene extends Phaser.Scene {
     // edward-maze [maze]
     // su-huan-li-maze [maze]
 
-    this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.spaceBar = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
     // Set the scale to cover the entire game width and height
     var background = this.add.image(0, 0, "background");
-    background.setScale(config.width / background.width, config.height / background.height);
+    background.setScale(
+      config.width / background.width,
+      config.height / background.height
+    );
     background.setOrigin(0);
     //Christian images
-    this.add.image(700, 700, 'apple-1');
-    this.add.image(750, 700, 'bag-0');
-    this.add.image(800, 700, 'book-0');
-    this.add.image(850, 700, 'cauldron-0');
-    this.add.image(900, 700, 'chest-0');
-    this.add.image(950, 700, 'clock-0');
-    this.add.image(1000, 700, 'feather-0');
-    this.add.image(1050, 700, 'horn-0');
-    this.add.image(1100, 700, 'pile-0');
-    this.add.image(1150, 700, 'rat-0');
-    this.add.image(1200, 700, 'red-skull-0');
-    this.add.image(1250, 700, 'torch-0');
-
+    this.add.image(700, 700, "apple-1");
+    this.add.image(750, 700, "bag-0");
+    this.add.image(800, 700, "book-0");
+    this.add.image(850, 700, "cauldron-0");
+    this.add.image(900, 700, "chest-0");
+    this.add.image(950, 700, "clock-0");
+    this.add.image(1000, 700, "feather-0");
+    this.add.image(1050, 700, "horn-0");
+    this.add.image(1100, 700, "pile-0");
+    this.add.image(1150, 700, "rat-0");
+    this.add.image(1200, 700, "red-skull-0");
+    this.add.image(1250, 700, "torch-0");
 
     //glowing images
-    this.add.image(600, 400, 'apple-0');
-    this.add.image(650, 400, 'bag');
-    this.add.image(700, 400, 'book');
-    this.add.image(750, 400, 'cauldron');
-    this.add.image(800, 400, 'chest');
-    this.add.image(850, 400, 'clock');
-    this.add.image(900, 400, 'feather-quill');
-    this.add.image(950, 400, 'horn');
-    this.add.image(1000, 400, 'pile');
-    this.add.image(1050, 400, 'rat');
-    this.add.image(1100, 400, 'red-skull');
-    this.add.image(1150, 400, 'torch');
+    this.add.image(600, 400, "apple-0");
+    this.add.image(650, 400, "bag");
+    this.add.image(700, 400, "book");
+    this.add.image(750, 400, "cauldron");
+    this.add.image(800, 400, "chest");
+    this.add.image(850, 400, "clock");
+    this.add.image(900, 400, "feather-quill");
+    this.add.image(950, 400, "horn");
+    this.add.image(1000, 400, "pile");
+    this.add.image(1050, 400, "rat");
+    this.add.image(1100, 400, "red-skull");
+    this.add.image(1150, 400, "torch");
     //player code
     this.soundMove = this.sound.add("soundMove", { volume: 1 });
 
-    this.player = this.physics.add.image(sizes.width - 300, sizes.height - 300, "theplayer").setOrigin(0, 0)
+    this.player = this.physics.add
+      .image(sizes.width - 300, sizes.height - 300, "theplayer")
+      .setOrigin(0, 0);
     // this.player.rotation()
-    this.player.body.allowGravity = false
+    this.player.body.allowGravity = false;
     this.player.setCollideWorldBounds(true);
     //keyboard movement for player testing
     this.cursor = this.input.keyboard.createCursorKeys();
     //background audio
-    this.bgMusic = this.sound.add('bg-audio');
+    this.bgMusic = this.sound.add("bg-audio");
     // this.bgMusic.play();
 
     //template for object/asset hitbox
-    this.object = this.physics.add.image(sizes.width - 300, sizes.height - 300, "apple").setOrigin('50%', '50%');
+    this.object = this.physics.add
+      .image(sizes.width - 300, sizes.height - 300, "apple")
+      .setOrigin("50%", "50%");
     this.object.setCollideWorldBounds(true);
     this.object.body.allowGravity = false;
     this.object.setImmovable(true);
     //line 39 is the template for the hitbox of an object/asset
     // this.object.setSize(this.object.width / 10, this.object.height - this.object.height / 10);
 
-<<<<<<< HEAD
     this.physics.add.collider(this.player, this.object);
-    this.add.image(0, 0, "bg").setOrigin(0, 0);
     this.player.scale = 0.5;
-=======
-   this.physics.add.collider(this.player, this.object);
-   this.player.scale = 0.5;
->>>>>>> e11ae36 (added a small textblock)
 
     this.soundMove.setVolume(1);
     this.soundMove.play();
 
-<<<<<<< HEAD
-    //circle color changes when clicked
+    //when circle is clicked, color changes and text appears
     var colors = [0xff0000, 0x00ff00]; // Red and Green
-    var currentIndex = 1; // Start with green
-    var circle = this.add.circle(100, 200, 25, colors[currentIndex]).setInteractive();
-    circle.on('pointerdown', function () {
+    var currentIndex = 0; // Start with red
+    var textBlock;
+    var backgroundRect;
+    var scene = this;
+    var circle = this.add
+      .circle(700, 200, 25, colors[currentIndex])
+      .setInteractive();
+    var textBlockX = 750;
+    var textBlockY = 200;
+
+    circle.on("pointerdown", function () {
+      // Toggle color
       currentIndex = (currentIndex + 1) % colors.length;
       circle.fillColor = colors[currentIndex];
+      // Toggle text block visibility
+      if (currentIndex === 1) {
+        if (!textBlock) {
+          // Create text block if not exists
+          textBlock = scene.add.text(
+            textBlockX,
+            textBlockY,
+            "Hello, this is a text block!",
+            { fontSize: "18px", fill: "#fff" }
+          );
+          textBlock.setVisible(false);
+          // Create a semi-transparent rectangle behind the text
+          backgroundRect = scene.add.rectangle(
+            textBlockX - 10,
+            textBlockY - 10,
+            textBlock.width + 20,
+            textBlock.height + 20,
+            0x000000,
+            0.7
+          );
+          backgroundRect.setOrigin(0, 0);
+          backgroundRect.setVisible(false);
+        }
+        textBlock.setVisible(!textBlock.visible);
+        // Toggle background rectangle visibility along with the text block
+        backgroundRect.setVisible(textBlock.visible);
+      } else {
+        // If the circle is red, hide the text block and background rectangle
+        if (textBlock) {
+          textBlock.setVisible(false);
+          backgroundRect.setVisible(false);
+        }
+      }
     });
-
-=======
-   //when circle is clicked, color changes and text appears
-   var colors = [0xff0000, 0x00ff00]; // Red and Green
-   var currentIndex = 0; // Start with red
-   var textBlock;
-   var backgroundRect;
-   var scene = this;  
-   var circle = this.add.circle(700, 200, 25, colors[currentIndex]).setInteractive();
-   var textBlockX = 750
-   var textBlockY = 200
-   
-   circle.on('pointerdown', function () {
-       // Toggle color
-       currentIndex = (currentIndex + 1) % colors.length;
-       circle.fillColor = colors[currentIndex];
-       // Toggle text block visibility
-       if (currentIndex === 1) {
-           if (!textBlock) {
-               // Create text block if not exists
-               textBlock = scene.add.text(textBlockX, textBlockY, "Hello, this is a text block!", { fontSize: '18px', fill: '#fff' });
-               textBlock.setVisible(false);
-               // Create a semi-transparent rectangle behind the text
-               backgroundRect = scene.add.rectangle(textBlockX - 10, textBlockY - 10, textBlock.width + 20, textBlock.height + 20, 0x000000, 0.7);
-               backgroundRect.setOrigin(0, 0);
-               backgroundRect.setVisible(false);
-           }
-           textBlock.setVisible(!textBlock.visible);
-           // Toggle background rectangle visibility along with the text block
-           backgroundRect.setVisible(textBlock.visible);
-       } else {
-           // If the circle is red, hide the text block and background rectangle
-           if (textBlock) {
-               textBlock.setVisible(false);
-               backgroundRect.setVisible(false);
-           }
-       }
-   });
->>>>>>> e11ae36 (added a small textblock)
   }
 
   //my player controls for testing using keyboard
   update() {
-
     const playerVolume = this.soundMove.volume;
 
-    if (typeof this.cursor !== 'undefined') {
+    if (typeof this.cursor !== "undefined") {
       const { left, right, up, down } = this.cursor;
 
       if (left.isDown) {
@@ -213,53 +218,49 @@ class GameScene extends Phaser.Scene {
         // rotate player to assign direction
         this.player.angle = 270;
         if (playerVolume !== 1) {
-          this.soundMove.setVolume(1)
+          this.soundMove.setVolume(1);
         }
       } else if (right.isDown) {
         this.player.setVelocityX(this.playerSpeed);
         // rotate player to assign direction
         this.player.angle = 90;
         if (playerVolume !== 1) {
-          this.soundMove.setVolume(1)
+          this.soundMove.setVolume(1);
         }
       } else {
         this.player.setVelocityX(0);
-        this.soundMove.setVolume(0)
+        this.soundMove.setVolume(0);
       }
       if (up.isDown) {
         this.player.setVelocityY(-this.playerSpeed);
         // if the left, right and up key is pressed same time then rotate player to assign direction
         if (left.isDown) {
           this.player.angle = 315;
-        }
-        else if (right.isDown) {
+        } else if (right.isDown) {
           this.player.angle = 45;
-        }
-        else {
+        } else {
           this.player.angle = 0;
         }
         if (playerVolume !== 1) {
-          this.soundMove.setVolume(1)
+          this.soundMove.setVolume(1);
         }
       } else if (down.isDown) {
         this.player.setVelocityY(this.playerSpeed);
         // if the left, right and down key is pressed same time then rotate player to assign direction
         if (left.isDown) {
           this.player.angle = 225;
-        }
-        else if (right.isDown) {
+        } else if (right.isDown) {
           this.player.angle = 135;
-        }
-        else {
+        } else {
           this.player.angle = 180;
         }
         if (playerVolume !== 1) {
-          this.soundMove.setVolume(1)
+          this.soundMove.setVolume(1);
         }
       } else {
         this.player.setVelocityY(0);
         if (playerVolume !== 0) {
-          this.soundMove.setVolume(0)
+          this.soundMove.setVolume(0);
         }
       }
     }
@@ -301,7 +302,6 @@ class GameScene extends Phaser.Scene {
     }
   }
 }
-
 
 const config = {
   type: Phaser.WEBGL,
